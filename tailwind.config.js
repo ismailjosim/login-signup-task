82ef0e87b11bb2c038c332bea9a5ebefad3972dc
JC DESIGN <ismailjosim99@gmail.com>
2022-12-02 23:46:35 +0600

    add tailwind css and google font

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#1678CB",
+
+          "secondary": "#3B8BEA ",
+
+          "accent": "#68D6F5",
+
+          "neutral": "#242424",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#4B8EEC",
+
+          "success": "#17968B",
+
+          "warning": "#F5C238",
+
+          "error": "#E8646D",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}

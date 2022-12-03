e7233cf787d7d4d8bc3420bd8ff5ef2fab04de73
JC DESIGN <ismailjosim99@gmail.com>
2022-12-03 23:19:25 +0600

    make site responsive

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -6,17 +6,17 @@ const Form = () => {
     const { personName } = useContext(FormContext);
 
     return (
-        <div className="h-screen flex">
-            <div className="grid lg:grid-cols-2 justify-between items-center gap-32">
+        <div className="h-screen mt-2 flex">
+            <div className="grid lg:grid-cols-2 justify-between items-center lg:gap-32">
                 <div className="text-center lg:text-left flex flex-col">
-                    <div>
+                    <div className='mt-0'>
                         <img src="logo.png" alt="" />
                     </div>
                     <div>
                         <img src="formImg.png" alt="" />
                     </div>
                 </div>
-                <div className="py-48 shadow-2xl bg-base-100">
+                <div className="shadow-2xl bg-base-100 py-10 lg:py-28">
                     <div className="card-body">
                         <div className="form-control border-b-2 mb-4">
                             <input type="text" placeholder="Write First Name" className="input border-none outline-none hover:border-none hover:outline-none" />
@@ -25,7 +25,7 @@ const Form = () => {
                             <input type="text" placeholder="Write First Name" className="input border-none outline-none hover:border-none hover:outline-none" />
                         </div>
                         <div className="mx-auto">
-                            <button className="btn btn-primary text-white flex gap-3 rounded-2xl">
+                            <button className="btn btn-primary text-white flex gap-3 rounded-2xl mt-3">
                                 <span>Next Step</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
